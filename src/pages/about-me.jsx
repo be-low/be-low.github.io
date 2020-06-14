72d42ac2b972d775940c2a864032f1094fc1f945
billow <billow.fun@gmail.com>
2020-06-14 14:06:01 +0800

    Add about

--- src/pages/about-me.jsx
+++ src/pages/about-me.jsx
@@ -1,13 +1,27 @@
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import React from 'react'
+import './about-me.css'
 
-export default function AboutMePage () {
+export default function AboutMePage() {
   return (
     <Layout>
-      <SEO title="AboutMe"/>
-      <h1>About Me</h1>
-      <p>It should write down something.</p>
+      <SEO title="AboutMe" />
+      <div>
+        <h2>About Me</h2>
+        <p>计算机科学初心者，但我可能不是真的喜欢？太多的 bug 常常会令人非常沮丧（无论是自己写的还是别人写的被我遇到了）</p>
+        <p>偶尔看一些电影，动漫，玩一些游戏</p>
+        <p>以前经常听周杰伦，现在也常听</p>
+        <p>很久之前玩了 To The Moon，所以有了现在的 ID</p>
+      </div>
+      <div>
+        <h2>Find Me</h2>
+        <p>Email: ???</p>
+        <p className='media'>
+          <a href='https://github.com/imbillow'>GitHub</a>
+          <a href='https://steamcommunity.com/id/__billow__/'>Steam</a>
+        </p>
+      </div>
     </Layout>
   )
 }
