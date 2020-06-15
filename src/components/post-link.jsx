aa6baa63ec736db3c9676f4aa5bd7111b329d62d
billow <billow.fun@gmail.com>
2020-06-15 20:26:11 +0800

    Style

--- src/components/post-link.jsx
+++ src/components/post-link.jsx
@@ -5,7 +5,7 @@ export default function PostsLink({ post }) {
   const { frontmatter } = post
   const path = `/post/${frontmatter.title}`
   return (
-    <Link style={{ display: 'flex', margin: '20px 0' }} to={path}>
+    <Link style={{ display: 'flex', padding: '20px 10px' }} to={path}>
       <span style={{ fontWeight: 'bold' }}>{frontmatter.title}</span>
       <span style={{ flex: '1 1 0' }}></span>
       <span style={{ fontWeight: 'lighter' }}>{frontmatter.date}</span>
