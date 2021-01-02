a939aed15226adc59183fb2b4b4d5eef056c6fdf
billow <billow.fun@gmail.com>
2021-01-02 21:28:56 +0800

    Oh about me

--- src/pages/about-me.jsx
+++ src/pages/about-me.jsx
@@ -11,21 +11,23 @@ export default function AboutMePage() {
         <h2>About Me</h2>
         <p>
           计算机科学初心者，但我可能不是真的喜欢？太多的 bug
-          常常会令人非常沮丧（无论是自己写的还是别人写的被我遇到了）
+          常常会令人非常沮丧（无论是自己写的还是遇到的别人写的）
         </p>
         <p>偶尔看一些电影，动漫，玩一些游戏</p>
         <p>以前经常听周杰伦，现在也常听</p>
-        <p>很久之前玩了 To The Moon，所以有了现在的 ID</p>
+        <p>不想把所有都写在这里（</p>
       </div>
       <div>
         <h2>Find Me</h2>
-        <p>Email: ???</p>
+        <p>Email: YmlsbG93LmZ1bkBnbWFpbC5jb20K</p>
         <p className="media">
           GitHub: <a href="https://github.com/imbillow">@billow</a>
         </p>
         <p>
           Steam: <a href="https://steamcommunity.com/id/__billow__/">@billow</a>
         </p>
+        <p>Twitter: <a href="https://twitter.com/o_o0_1">星之彩 [星空]</a></p>
+        <p>Telegram: <a href="https://t.me/imbillow">billow</a></p>
       </div>
     </Layout>
   )
